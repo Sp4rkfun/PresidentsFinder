14861497bb6db439134e63065c5fc21177521273
Alex Crowley <acrow94@gmail.com>
2015-11-03 17:53:01 -0500

    Started parsing html.

--- src/main.java
+++ src/main.java
@@ -1,10 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StanfordCoreNLP pipeline = new StanfordCoreNLP();
+		//StanfordCoreNLP pipeline = new StanfordCoreNLP();
+		File folder = new File("docs");
+		File[] files = folder.listFiles();
+		try {
+		for(File file:files){
+			Document doc = Jsoup.parse(file,"ASCII");
+			Element title = doc.select("title").first();
+			String name = title.text();
+			Pattern p = Pattern.compile(" -");
+			Matcher m = p.matcher(name);
+			if(m.find())System.out.println(name.substring(0,m.start()));
+			Elements divs = doc.select("div");
+			for(Element e:divs){
+				System.out.println(e.text());
+			}
+		}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
