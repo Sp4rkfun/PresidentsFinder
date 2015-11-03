f90e842446ddd3e9bf7cfd7dc49897045b1b95fe
Alex Crowley <acrow94@gmail.com>
2015-11-02 19:51:52 -0500

    Initial Import

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StanfordCoreNLP pipeline = new StanfordCoreNLP();
+	}
+
+}
